c5cd8c8776432bc3aedd9a3a3861c53d5a2ab945
Danny <dannyvdweijde@gmail.com>
2023-02-15 20:04:44 +0100

    Finished styling the socials bar for desktop.

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -6,12 +6,16 @@
                     <span class="socials__title">
                         Check out the freebies, especially made for you
                     </span>
-                    <font-awesome-icon class="socials__icon" icon="fa-solid fa-angles-right" />
+                    <div class="socials__icon-container">
+                        <font-awesome-icon class="socials__icon" icon="fa-solid fa-angles-right" />
+                    </div>
                 </div>
                 <nav class="socials__nav">
                     <ul class="socials__list">
                         <li class="socials__list-item" v-for="(social, index) of socials" :key="index">
-                            <a class="socials__link" :href="social.url" target="_new" :title="index">
+                            <a :class="['socials__link', social.modifier ?? '']" :href="social.url" target="_new" 
+                                :title="`${index[0].toUpperCase()}${index.substring(1)}`"
+                            >
                                 <img class="socials__img" :src="social.img" :alt="index" loading="lazy">
                             </a>
                         </li>
@@ -46,24 +50,28 @@ export default {
                     'img': require('@/assets/images/dvoted-mail.webp'),
                 },
 
-                insta: {
+                instagram: {
                     'url': 'https://www.instagram.com/ddvoted/',
-                    'img': require('@/assets/images/dvoted-insta.webp'),
+                    'img': require('@/assets/images/dvoted-instagram.webp'),
+                    'modifier': 'socials__link--instagram'
                 },
 
                 pinterest: {
                     'url': 'https://nl.pinterest.com/',
                     'img': require('@/assets/images/dvoted-pinterest.webp'),
+                    'modifier': 'socials__link--pinterest'
                 },
 
                 spotify: {
                     'url': 'https://open.spotify.com/playlist/3x3Log1AV65f9ejgTEUBDF?si=aOaRl0bPRKCyz4f4KShf0Q&amp;nd=1',
                     'img': require('@/assets/images/dvoted-spotify.webp'),
+                    'modifier': 'socials__link--spotify'
                 },
 
                 youtube: {
                     'url': 'https://www.youtube.com/channel/UC-q4017C2MlgcwNiI8UmBzg',
                     'img': require('@/assets/images/dvoted-youtube.webp'),
+                    'modifier': 'socials__link--youtube'
                 },
             }
         }
